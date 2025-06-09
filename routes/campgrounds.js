de0657f3a3770a56683cdcd12a6c1ab5fd769392
snehilhota <snehilhota13032006@gmail.com>
2025-06-09 23:43:38 +0530

    Added image upload part1

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
+
 const campgrounds = require('../controllers/campgrounds');
 
 const catchAsync = require('../utils/catchAsync');
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-    
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
+
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
 router.route('/:id')
--- schemas.js
+++ schemas.js
@@ -4,7 +4,7 @@ module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
         title: Joi.string().required(),
         location: Joi.string().required(),
-        image: Joi.string().required(),
+        // image: Joi.string().required(),
         price: Joi.number().required().min(0),
         description: Joi.string().required()
     })
